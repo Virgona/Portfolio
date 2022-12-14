cb7fba2277d57122ae17ad53b4191bf07ac569e1
Tobias Virgona <tobiasvirgona@gmail.com>
2022-12-14 20:35:35 +1100

    using the skeleton of a class activitey to make the React apps with a nav bar and pages. Customized the home page to be about me with a bio

---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>About Me:</h1>
+      <p>
+        Hello traveller, my name is Tobias. I am a 30 year old aspiring full-stack developer, podcast host, gamer and all round good dude.
+        I started learning some coding basics in 2021 and found myself falling in love, so in 2022 I signed up for a coding bootcamp to further hone some skills!
+
+        Film is my first love, I have studied Screen and Media at tafe which covered all sorts of filmmaking - writing scripts, cinematography, editing, practical effects and VFX.
+        If I am not expanding my coding brain I will usually be watching a movie and talking about it on my podcast <a target='blank' href='https://directory.libsyn.com/shows/view/id/forevermovieboys'>Forever Movie Boys</a>
+        or you can find me playing League of Legends - like a disgustingly sweaty, sweater
+      </p>
+    </div>
+  );
+}
