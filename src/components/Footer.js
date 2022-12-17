629f7b9e89b2698a944c35f9b3c555da809c9c4c
Tobias Virgona <tobiasvirgona@gmail.com>
2022-12-17 13:15:20 +1100

    footer added with stylings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin, } from '@fortawesome/free-brands-svg-icons'
-import { faPodcast } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faSpotify } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
     // const footerStyles = {
@@ -15,10 +14,10 @@ export default function Footer() {
 
     // }
     return (
-        <div calssName='footerStyles'>
-            <p><FontAwesomeIcon icon={faGithub} /></p>
-            <p><FontAwesomeIcon icon={faLinkedin} /></p>
-            <p><FontAwesomeIcon icon={faPodcast} /></p>
-        </div>
+        <footer className='footerStyles'>
+            <p className='iconStyles' id='git'><a target='blank' href='https://github.com/Virgona'><FontAwesomeIcon icon={faGithub} /></a> </p>
+            <p className='iconStyles' id='linkedin'><a target='blank' href='https://www.linkedin.com/in/tobias-virgona/'><FontAwesomeIcon icon={faLinkedin} /></a></p>
+            <p className='iconStyles' id='podcast'><a target='blank' href='https://open.spotify.com/show/6qb1PFFEuiBMShVY2YTocK'><FontAwesomeIcon icon={faSpotify} /></a></p>
+        </footer>
     );
 }
