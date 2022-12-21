bc4ebfadcb7e3572b19ad695c4c76af6e16c2578
Tobias Virgona <tobiasvirgona@gmail.com>
2022-12-21 15:48:32 +1100

    working on the validation for contact. All fields are triggering at once at the moment

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css'
 
+
+
 export default function Contact() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleNameChange = evt => {
+    setName(evt.target.value);
+  };
+  const handleEmailChange = evt => {
+    setEmail(evt.target.value);
+  };
+  const handleMessageChange = evt => {
+    setMessage(evt.target.value);
+  };
+  const isNameValid = name !== '';
+  const isEmailValid = email !== '';
+  const isMessageValid = message !== '';
+  const [touched, setTouched] = useState(false);
 
   return (
     <div>
-      <h1>Contact Me</h1>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-">
+      <h2 className='headingPosition'>Contact Me:</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-">
             <div className="form-floating mb-3">
-              <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" />
+              <input type="text" class="form-control" id="floatingInput" value={name} onChange={handleNameChange} onBlur={() => setTouched(true)} />
               <label for="floatingInput">Name</label>
+              <div>
+                {touched ? (isNameValid ? " " : "Name Required") : null}
+              </div>
             </div>
             <div className="form-floating mb-3">
-              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" value={email} onChange={handleEmailChange} onBlur={() => setTouched(true)} />
               <label for="floatingInput">Email address</label>
+              <div>
+                {touched ? (isEmailValid ? " " : "Email Required") : null}
+              </div>
             </div>
-            <div class="form-floating">
-              <textarea class="form-control textareaStyles" placeholder="Send me some good news!" id="floatingTextarea2"></textarea>
+            <div className="form-floating">
+              <textarea className="form-control textareaStyles" placeholder="Send me some good news!" id="floatingTextarea2" value={email} onChange={handleMessageChange} onBlur={() => setTouched(true)} ></textarea>
               <label for="floatingTextarea2">Tell me something good!</label>
-              <button type="button" class="btn btn-outline-success">Send Message</button>
+              <div>
+                {touched ? (isMessageValid ? " " : "Message Required") : null}
+              </div>
+              <button type="button" className="btn btn-outline-success">Send Message</button>
             </div>
           </div>
         </div>
