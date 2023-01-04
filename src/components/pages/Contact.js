42b19c4552515e4d4648ff24415eaeb838590085
Tobias Virgona <tobiasvirgona@gmail.com>
2023-01-04 15:01:04 +1100

    changed some of the toast and redeployed to pages

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -77,8 +77,11 @@ export default function Contact() {
                   {touchedMessage ? (isMessageValid ? " " : "Message Required") : null}
                 </div>
                 <button type="submit" value="Send" className="btn btn-outline-success">Send Message</button>
-                {!toast ? null : <div class="alert alert-primary" role="alert">
-                  Email Sent
+                {!toast ? null : <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                  <strong>Email Sent! Thanks for reaching out</strong>
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
                 </div>}
               </div>
             </div>
